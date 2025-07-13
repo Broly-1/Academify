d6c2f03b6c5d2d05867f887497b766344bda57fc
Hassan <hassangaming111@gmail.com>
2025-07-13 17:58:03 +0500

    deployed on web

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,10 @@ import 'constants/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MaterialApp(
       title: 'Academify School Center',
